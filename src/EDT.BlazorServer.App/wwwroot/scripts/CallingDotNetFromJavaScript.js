d74b9c5bf3fd1df3706bbf818c6f0adaeb778c15
edisonzhou <edisonchou@xique.com>
2023-12-26 23:21:04 +0800

    Add Sample

---
+++ src/EDT.BlazorServer.App/wwwroot/scripts/CallingDotNetFromJavaScript.js
@@ -0,0 +1,8 @@
+var EdisonTalkUtil = EdisonTalkUtil || {};
+EdisonTalkUtil.startRandomGenerator = function (dotNetObject) {
+    setInterval(function () {
+        let text = Math.random() \* 1000;
+        console.log("JS: Generated " + text);
+        dotNetObject.invokeMethodAsync('AddText', text.toString());
+    }, 1000);
+};
